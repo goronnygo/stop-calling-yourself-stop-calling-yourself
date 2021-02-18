184356c6122a635bf1bd188de669ad63907d775b
Ronny <ronnykuruvilla@gmail.com>
2021-02-18 15:50:46 -0500

    Complete.

---
+++ main.js
@@ -0,0 +1,75 @@
+// TASK ONE//
+function task1(str){
+    return str + '???';
+}
+
+const h = task1('hello')
+h;
+
+const w = task1('what even');
+w;
+
+
+//TASK TWO//
+function task2(str, str2){
+    return str + " " + str2;
+}
+const words = task2('oh', 'hi')
+words;
+
+const moreWords = task2('well', 'hello back')
+moreWords;
+
+
+//TASK THREE//
+
+function task3(num, num2, num3){
+    return num - num2 - num3;
+}
+
+const firstEquation = task3('10', '1', '2')
+firstEquation;
+
+const secondEquation = task3('8', '3', '5')
+secondEquation;
+
+const thirdEquation =task3('100', '80', '25')
+thirdEquation;
+
+
+//TASK FOUR//
+function task4(num, num2, num3){
+    return num3 - num2 - num;
+}
+
+const equationOne =  task4('10', '1', '2')
+equationOne;
+
+const equationTwo = task4('2', '1', '10')
+equationTwo;
+
+
+//TASK FIVE//
+function task5(str){
+    return str + str + str;
+}
+
+const email = task5('codeimmersives');
+email; 
+const best = task5('is the best')
+best;
+
+
+//TASK 6//
+function task6(num){
+    return Math.sqrt(num) ;
+}
+
+const problemOne = task6('16')
+problemOne;
+
+const problemTwo = task6('4')
+problemTwo;
+
+const problemThree = task6('9')
+problemThree;
